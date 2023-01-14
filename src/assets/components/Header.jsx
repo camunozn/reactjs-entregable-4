64f45d147f6fe0a0b6f766abc54d96d1b00ecc31
camunozn <camunozn89@gmail.com>
2023-01-14 06:59:59 -0500

    added basic html and components structure

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="heading-primary">Usuarios</h1>
+      <button className="btn btn--user"></button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/assets/components/UsersSearch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UsersSearch = () => {
+  return <div></div>;
+};
+
+export default UsersSearch;
