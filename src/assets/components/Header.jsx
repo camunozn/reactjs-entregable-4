f98627eb8df642972627102d3886378970144fa6
camunozn <camunozn89@gmail.com>
2023-01-17 19:39:58 -0500

    minor content changes

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ toggleIsHidden }) => {
   return (
     <div className="header flex">
-      <h1 className="heading-primary">Usuarios</h1>
+      <h1 className="heading-primary">Users</h1>
       <button className="btn btn--user" onClick={toggleIsHidden}>
         + create new user
       </button>
